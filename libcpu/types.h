28bf6a55382bf3d775fb5d5819d64d699eea846a
Nathan Jackson <nate.ds.jackson@gmail.com>
2015-01-18 16:54:36 -0500

    Fixed compilation issues.

--- libcpu/types.h
+++ libcpu/types.h
@@ -3,6 +3,7 @@
 
 #include <sys/types.h>
 #include "defines.h"
+#include <stdint.h>
 
 typedef signed char sint8_t;
 typedef signed short sint16_t;
@@ -13,7 +14,7 @@ typedef signed long long sint64_t;
 typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
 typedef unsigned int uint32_t;
-typedef unsigned long long uint64_t;
+//typedef unsigned long long uint64_t;
 #endif
 
 typedef uint64_t addr_t;
